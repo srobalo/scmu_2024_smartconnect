7c693db17bd8fa849cf449cf058d16c0d1bb45c5
Diogo Ye <81536043+Darkhark19@users.noreply.github.com>
2024-05-23 19:34:03 +0100

    Adding capabilities scene, Missing field to add persoan

--- lib/screens/devices_screen.dart
+++ lib/screens/devices_screen.dart
@@ -192,34 +192,54 @@ class _DevicesScreenState extends State<DevicesScreen>
                 );
               },
             ),
-      Stack(children: [
-        Align(
+      Positioned(
+          bottom: 16.0,
+          right: 16.0,
+          child: Align(
             alignment: Alignment.bottomCenter,
             child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-              Positioned(
-                bottom: 16.0,
-                right: 16.0,
-                child: FloatingActionButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.person),
-                ),
+              FloatingActionButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false, // user must tap button!
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('AlertDialog Title'),
+                        content: const SingleChildScrollView(
+                          child: ListBody(
+                            children: <Widget>[
+                              Text('This is a demo alert dialog.'),
+                              Text(
+                                  'Would you like to approve of this message?'),
+                            ],
+                          ),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: const Text('Approve'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Icon(Icons.person),
               ),
-              Positioned(
-                bottom: 16.0,
-                right: 16.0,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AddDeviceScreen()),
-                    );
-                  },
-                  child: const Icon(Icons.add),
-                ),
+              FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AddDeviceScreen()),
+                  );
+                },
+                child: const Icon(Icons.add),
               ),
-            ])),
-      ])
+            ]),
+          )),
     ]);
   }
 }
